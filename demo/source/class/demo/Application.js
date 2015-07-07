a2b56a43624fa369b35c82b29cf8a486a53aac32
Andriy Syrovenko <andriys@gmail.com>
2015-07-08 00:49:02 +0300

    Made demo application interactive

--- demo/source/class/demo/Application.js
+++ demo/source/class/demo/Application.js
@@ -14,21 +14,53 @@ qx.Class.define("demo.Application",
         qx.log.appender.Console;
       }
 
+      var text = new qx.ui.form.TextArea("Sample QR Symbol.").set({
+        width: 300,
+        height: 150
+      });
+
+      var label = new qx.ui.basic.Label("Error correction level: ");
+      var ecLevel = new qx.ui.form.SelectBox().set({ width: 80 });
+      label.setBuddy(ecLevel);
+      ecLevel.add(new qx.ui.form.ListItem("L (7%)", null, "L"));
+      ecLevel.add(new qx.ui.form.ListItem("M (15%)", null, "M"));
+      ecLevel.add(new qx.ui.form.ListItem("Q (25%)", null, "Q"));
+      ecLevel.add(new qx.ui.form.ListItem("H (30%)", null, "H"));
+      ecLevel.setSelection([ ecLevel.getSelectables()[1] ]);
+
+      var button = new qx.ui.form.Button("Update Symbol").set({ width: 100 });
+
       var canvas = new qx.ui.embed.Canvas().set({
-        width: 114,
-        height: 114,
         syncDimension: true
       });
 
-      var qr = new qrcode.QRCode(
-        "otpauth://totp/example:Корп.%20Монстров?secret=GEZDGNBVGY3TQOJQMFRGGZDFMZTWQ2LK&issuer=Корп.%20Монстров");
+      var qr = new qrcode.QRCode();
+
+      var updateSymbol = function() {
+        qr.clear();
+        qr.addSegment(text.getValue());
+        qr.setEcLevel(ecLevel.getSelection()[0].getModel());
+        canvas.set({
+          width: qr.getImageSize(),
+          height: qr.getImageSize()
+        });
+        canvas.update();
+      };
 
       canvas.addListener("redraw", function(e) {
         qr.draw(e.getData().context);
       });
 
+      button.addListener("execute", updateSymbol);
+
       var doc = this.getRoot();
-      doc.add(canvas, {left: 400, top: 50});
+      doc.add(text, {left: 20, top: 20});
+      doc.add(label, {left: 20, top: 180});
+      doc.add(ecLevel, {left: 130, top: 175});
+      doc.add(button, {left: 220, top: 175});
+      doc.add(canvas, {left: 340, top: 20});
+
+      updateSymbol();
     }
   }
 });
